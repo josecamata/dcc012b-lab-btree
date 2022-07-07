f63aa34a537c00e18a8219751ccf6792fcb339c2
Jose Camata <camata@ice.ufjf.br>
2022-07-07 11:12:33 -0300

    first commit

---
+++ btree.h
@@ -0,0 +1,32 @@
+#ifndef BTREE_H__
+#define BTREE_H__
+
+//-----------------------------------------------------------------------------
+// B-Tree implementation
+// 
+
+template<typename T> class BTree;
+
+// TODO: Implementar a  classe BTreeNode
+template<typename T>
+class BTreeNode {
+
+};
+
+// TODO: Implementar a classe BTree
+//  Os seguintes métodos deve ser implementados:
+//   - BTree(int ordem): Cria uma Btree com a ordem passada como parâmetro.
+//   - ~BTree(): Destrutor da Btree.
+//   - void insert(T element): Insere o elemento passado como parâmetro na Btree.
+//   - void remove(T element): Remove o elemento passado como parâmetro da Btree.
+//   - bool search(T element): Retorna true se o elemento passado como parâmetro está na Btree, false caso contrário.
+//   - void print(): Imprime a Btree in-order
+template<typename T>
+class BTree {
+
+};
+
+
+
+
+#endif /* BTREE_H__ */
